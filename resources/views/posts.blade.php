bf1eddfbfdd67b6268eeb0d7625efe289ac6a7f5
Sergio1-Git <mercadosergio754@gmail.com>
2022-07-31 16:00:13 -0500

    php artisan storage:link (mostrar imagen de un post)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,17 +8,22 @@
 
             <div class="card">
                 <div class="card-body">
-                <h5 class="card-title">{{$post->title}}</h5>
-                <p class="card-text">
-                    {{ $post->get_excerpt }}
-                    <a href="{{ route('post', $post) }}">Leer más</a>
-                </p>
-                <p class="tex-muted mb-0">
-                    <em>
-                        &ndash; {{ $post->user->name }}
-                    </em>
-                    {{ $post->created_at->format('d M Y') }}
-                </p>
+                    <h5 class="card-title">{{$post->title}}</h5>
+                    <div class="form-group row">
+
+                            <img class="col-6 offset-3" src="{{url('storage/'.$post->image)}}" alt="">
+                            <input class="col-12 mt-4" type="file" name="file">
+                    </div>
+                    <p class="card-text">
+                        {{ $post->get_excerpt }}
+                        <a href="{{ route('post', $post) }}">Leer más</a>
+                    </p>
+                    <p class="tex-muted mb-0">
+                        <em>
+                            &ndash; {{ $post->user->name }}
+                        </em>
+                        {{ $post->created_at->format('d M Y') }}
+                    </p>
                 </div>
             </div>
             @endforeach

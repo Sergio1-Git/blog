18348f53771e0aaad6b5f48bd6d89d9dad77bf61
Sergio1-Git <mercadosergio754@gmail.com>
2022-08-01 14:52:27 -0500

    fix sintax error

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,12 +8,12 @@
             <div class="card mb-4">
                 @if($post->image)
                     <img class="card-img-top" src="{{ $post->get_image }}" alt="">
-                    @endif
+                @endif
                 <div class="card-body">
-                    <h5 class="card-title">{{$post->title}}</h5>
-                    if($post->iframe)
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    @if($post->iframe)
                     <div class="embed-responsive embed-responsive-16by9">
-                        {!! $post->iframe !! }
+                        {!! $post->iframe !!}
                     </div>
                     @endif
                     <p class="card-text">
